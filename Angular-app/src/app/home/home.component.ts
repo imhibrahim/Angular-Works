21ad53e5c3e963ce58dd644bbf1d04f216e76d2e
mohammad ibrahim <imh774077@gmail.com>
2024-02-29 22:42:56 +0500

    Firts Work In angular

---
+++ Angular-app/src/app/home/home.component.ts
@@ -0,0 +1,46 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+
+// //  list:string[]=["ibrahim","khizer","kutta","manhoos"]
+
+//  myfun():void
+//  {
+//   console.log("consollog ")
+  
+//  }
+
+// myvar:string=("this ibrahim");
+
+// dosumthing()
+// {
+//  this.myvar=("this is ibrahim full stack developer");
+// }
+
+// is_disabled:boolean=false
+
+
+// changestatus(){
+//    this.is_disabled=true
+// }
+
+
+//show:boolean= false;
+
+//color:string='Red';
+
+color:string[]=["red","green","blue","black","purple"]
+singelcolor="red"
+// color='red';
+
+
+
+
+
+
+}
